3a61772df71c83cde371f28f69f006dcf44cf324
kirill5k <immotional@aol.com>
2023-09-09 16:43:31 +0100

    String interpolation ex

--- hello.go
+++ hello.go
@@ -7,10 +7,10 @@ import (
 func main() {
 
 	//declare a var with a type
-	var helloStr string = "Hello, "
+	var helloStr string = "Hello, %s!"
 	//var declaration, only works inside functions
 	nameStr := "World"
 
-	fmt.Println(helloStr + nameStr + "!")
+	fmt.Println(fmt.Sprintf(helloStr, nameStr))
 	fmt.Printf("The type is %T\n", helloStr)
 }
