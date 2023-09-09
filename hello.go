831118b7bb632dffc9eaf3967582fe3bc4eadec0
kirill5k <immotional@aol.com>
2023-09-09 16:50:10 +0100

    Tidy up helloworld example

--- hello.go
+++ hello.go
@@ -5,14 +5,14 @@ import (
 	"time"
 )
 
+const (
+	HelloWorldTemplate string = "Hello, %s!"
+)
+
 func main() {
-
-	//declare a var with a type
-	var helloStr string = "Hello, %s!"
 	//var declaration, only works inside functions
-	nameStr := "World"
-
-	fmt.Println(fmt.Sprintf(helloStr, nameStr))
+	helloStr := fmt.Sprintf(HelloWorldTemplate, "Boris")
+	fmt.Println(helloStr)
 	fmt.Printf("The type is %T\n", helloStr)
 
 	now := time.Now()
