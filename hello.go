a434a2211a507f8653966ea275ffa61f95bdd7aa
kirill5k <immotional@aol.com>
2023-09-25 16:48:18 +0100

    Select

--- hello.go
+++ hello.go
@@ -353,7 +353,6 @@ func goChannels() {
 
 func goSelect() {
 	ch1, ch2 := make(chan int), make(chan int)
-
 	go func() {
 		ch1 <- 42
 	}()
@@ -363,5 +362,18 @@ func goSelect() {
 		fmt.Printf("Received value from ch1: %d\n", val)
 	case val := <-ch2:
 		fmt.Printf("Received value from ch2: %d\n", val)
+	}
+
+	chOut := make(chan float64)
+	go func() {
+		time.Sleep(100 * time.Millisecond)
+		chOut <- 3.14
+	}()
+
+	select {
+	case val := <-chOut:
+		fmt.Printf("Received %f\n", val)
+	case <-time.After(20 * time.Millisecond):
+		fmt.Println("timeout")
 	}
 }
