c142fb83bbe43c7f6967b9db672878527c4dc7d1
kirill5k <immotional@aol.com>
2023-10-29 15:48:18 +0000

    Builder pattern example

---
+++ internal/patterns/builder.go
@@ -0,0 +1,74 @@
+package patterns
+
+type Notification struct {
+	title    string
+	subtitle string
+	message  string
+	image    string
+	icon     string
+	priority int
+	kind     string
+}
+
+type NotificationBuilder struct {
+	Title    string
+	Subtitle string
+	Message  string
+	Image    string
+	Icon     string
+	Priority int
+	Kind     string
+}
+
+func NewNotificationBuilder() *NotificationBuilder {
+	return &NotificationBuilder{}
+}
+
+func (builder *NotificationBuilder) Build() (*Notification, error) {
+	notification := &Notification{
+		title:    builder.Title,
+		subtitle: builder.Subtitle,
+		message:  builder.Message,
+		image:    builder.Image,
+		icon:     builder.Icon,
+		priority: builder.Priority,
+		kind:     builder.Kind,
+	}
+
+	return notification, nil
+}
+
+func (builder *NotificationBuilder) SetTitle(title string) *NotificationBuilder {
+	builder.Title = title
+	return builder
+}
+
+func (builder *NotificationBuilder) SetSubTitle(subTitle string) *NotificationBuilder {
+	builder.Subtitle = subTitle
+	return builder
+}
+
+func (builder *NotificationBuilder) SetMessage(message string) *NotificationBuilder {
+	builder.Message = message
+	return builder
+}
+
+func (builder *NotificationBuilder) SetImage(image string) *NotificationBuilder {
+	builder.Image = image
+	return builder
+}
+
+func (builder *NotificationBuilder) SetIcon(icon string) *NotificationBuilder {
+	builder.Icon = icon
+	return builder
+}
+
+func (builder *NotificationBuilder) SetPriority(priority int) *NotificationBuilder {
+	builder.Priority = priority
+	return builder
+}
+
+func (builder *NotificationBuilder) SetKind(kind string) *NotificationBuilder {
+	builder.Kind = kind
+	return builder
+}
