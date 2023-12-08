810b4e1fd855dd47a40838086d1bf27405495a9a
kirill5k <immotional@aol.com>
2023-12-08 10:54:02 +0000

    In memory cache struct

--- internal/patterns/cache.go
+++ internal/patterns/cache.go
@@ -1,7 +1,22 @@
 package patterns
 
+import (
+	"sync"
+	"time"
+)
+
 type Cache[K any, V any] interface {
 	Get(key K) *V
 	Put(key K, value V)
 	Contains(key K) bool
+}
+
+type cacheEntry[V any] struct {
+	value V
+	time  time.Time
+}
+
+type inMemoryCache[K any, V any] struct {
+	mu     sync.Mutex
+	values map[string]cacheEntry[V]
 }
