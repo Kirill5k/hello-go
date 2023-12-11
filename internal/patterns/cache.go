b0526d455ea267c55f2c764660f727d0899d7e94
kirill5k <immotional@aol.com>
2023-12-11 13:19:04 +0000

    Make in memory cache exported

--- internal/patterns/cache.go
+++ internal/patterns/cache.go
@@ -17,24 +17,24 @@ type cacheEntry[V any] struct {
 	time  time.Time
 }
 
-type inMemoryCache[K comparable, V any] struct {
+type InMemoryCache[K comparable, V any] struct {
 	mu     sync.Mutex
 	values map[K]cacheEntry[V]
 }
 
-func NewInMemoryCache[K comparable, V any](expiresIn time.Duration, checkEvery time.Duration) Cache[K, V] {
-	cache := &inMemoryCache[K, V]{values: make(map[K]cacheEntry[V])}
+func NewInMemoryCache[K comparable, V any](expiresIn time.Duration, checkEvery time.Duration) *InMemoryCache[K, V] {
+	cache := &InMemoryCache[K, V]{values: make(map[K]cacheEntry[V])}
 	go deleteExpiredItems(cache, expiresIn, checkEvery)
 	return cache
 }
 
-func deleteExpiredItems[K comparable, V any](c *inMemoryCache[K, V], expiresIn time.Duration, checkEvery time.Duration) {
+func deleteExpiredItems[K comparable, V any](c *InMemoryCache[K, V], expiresIn time.Duration, checkEvery time.Duration) {
 	c.deleteExpiredItems(time.Now(), expiresIn)
 	time.Sleep(checkEvery)
 	deleteExpiredItems(c, expiresIn, checkEvery)
 }
 
-func (c *inMemoryCache[K, V]) Get(key K) *V {
+func (c *InMemoryCache[K, V]) Get(key K) *V {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	v, ok := c.values[key]
@@ -44,26 +44,26 @@ func (c *inMemoryCache[K, V]) Get(key K) *V {
 	return &v.value
 }
 
-func (c *inMemoryCache[K, V]) Contains(key K) bool {
+func (c *InMemoryCache[K, V]) Contains(key K) bool {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	_, ok := c.values[key]
 	return ok
 }
 
-func (c *inMemoryCache[K, V]) Put(key K, value V) {
+func (c *InMemoryCache[K, V]) Put(key K, value V) {
 	c.mu.Lock()
 	c.values[key] = cacheEntry[V]{value: value, time: time.Now()}
 	c.mu.Unlock()
 }
 
-func (c *inMemoryCache[K, V]) Size() int {
+func (c *InMemoryCache[K, V]) Size() int {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	return len(c.values)
 }
 
-func (c *inMemoryCache[K, V]) deleteExpiredItems(currentTime time.Time, expiresIn time.Duration) {
+func (c *InMemoryCache[K, V]) deleteExpiredItems(currentTime time.Time, expiresIn time.Duration) {
 	c.mu.Lock()
 	for k, v := range c.values {
 		if v.time.Add(expiresIn).After(currentTime) {
