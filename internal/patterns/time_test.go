9e92130931fc3263c4468ccd342abec26b758dd0
kirill5k <immotional@aol.com>
2024-01-31 11:00:25 +0000

    Add more tests

--- internal/patterns/time_test.go
+++ internal/patterns/time_test.go
@@ -10,10 +10,12 @@ func Test_IsLeapYear_ReturnFalseForNonLeapYear(t *testing.T) {
 	require.False(t, IsLeapYear(1800))
 	require.False(t, IsLeapYear(2001))
 	require.False(t, IsLeapYear(2100))
+	require.False(t, IsLeapYear(2500))
 }
 
 func Test_IsLeapYear_ReturnTrueNonLeapYear(t *testing.T) {
 	require.True(t, IsLeapYear(2000))
 	require.True(t, IsLeapYear(2004))
+	require.True(t, IsLeapYear(2016))
 	require.True(t, IsLeapYear(2400))
 }
